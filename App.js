91ab08a46aa24302c0e6b783deff8e017eb9c9dd
kushal-kamra <kushalkamradev@gmail.com>
2023-04-16 08:45:25 +0530

    Hello, linter and prettier

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  const message = "Hello";
+
+  return (
+    <View
+      style={{
+        height: 100,
+        width: 100,
+        backgroundColor: "orange",
+      }}
+    >
+      <Text>{message}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
