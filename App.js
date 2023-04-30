8d8788c78c8bb4b772e4c0ca61d9e8dd20a82f8e
kushal-kamra <kushalkamradev@gmail.com>
2023-04-30 10:11:47 +0530

    Adding api, refactoring

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, ActivityIndicator, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
+import { useGetWeather } from "./src/hooks/useGetWeather";
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
+  const [loading, error, weather] = useGetWeather();
+  console.log(loading, error, weather);
 
-  if (loading) {
+  if (weather && weather.list) {
     return (
-      <View style={styles.container}>
-        <ActivityIndicator size={"large"} color={"blue"} />
-      </View>
+      <NavigationContainer>
+        <Tabs weather={weather} />
+      </NavigationContainer>
     );
   }
 
   return (
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
+    <View style={styles.container}>
+      <ActivityIndicator size={"large"} color={"blue"} />
+    </View>
   );
 };
 
